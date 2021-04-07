fff47a0eee3fc64d7ecf5cee699a72aa7c0bdcfe
PC <oliveira.pc98@gmail.com>
2021-04-07 11:43:47 -0300

    URL model File sequelize fix

--- backend/src/App/models/File.js
+++ backend/src/App/models/File.js
@@ -6,7 +6,12 @@ const database = require('../../database/index');
 const File = database.define('File', {
     name: DataTypes.STRING,
     path: DataTypes.STRING,
-    url: DataTypes.STRING
+    url: {
+        type: DataTypes.VIRTUAL,
+        get () {
+            return `${process.env.APP_URL}:${process.env.APP_PORT}/files/${this.path}`
+        }
+    }
 });
 
 module.exports = File;
